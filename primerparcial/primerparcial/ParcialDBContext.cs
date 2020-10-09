d7bdeb0af935a91bb74027ab7ab8fe2c8bf9e633
MicaelaLagana <lagana.mcl@gmail.com>
2020-10-08 23:41:20 -0300

    se agregan los metodos del usuario

--- primerparcial/primerparcial/ParcialDBContext.cs
+++ primerparcial/primerparcial/ParcialDBContext.cs
@@ -9,7 +9,7 @@ namespace primerparcial
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=tareas.db");
+            optionsBuilder.UseSqlite("Data Source=parcial.db");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -50,5 +50,11 @@ namespace primerparcial
             .Property(p => p.Tiempo).HasMaxLength(30).IsRequired();
 
         }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Detalle> Detalles { get; set; }
+        public DbSet<Recurso> Recursos { get; set; }
+
     }
 }
--- primerparcial/primerparcial/Program.cs
+++ primerparcial/primerparcial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace primerparcial
 {
@@ -6,17 +7,103 @@ namespace primerparcial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserPost("Pepe", "1234");
+            UserPost("Moni", "4321");
+            UserPost("Paola", "1222");
+            UserPost("Coqui", "3241");
 
+            GetUsers();
+
+            PutUser(1, "Pepe", "Dardo");
+            PutUserByID(2, "Maria Elena");
+            PutUserByUsername("Paola", "La nena");
+
+            GetUsers();
+
+            DeleteUser(4);
+
+        }
+
+        static void UserPost(string user, string clave)
+        {
             var ctx = new ParcialDBContext();
 
-            ctx.Set<Usuario>().Add(new Usuario
+            ctx.Usuarios.Add(new Usuario
             {
-                Id = 1,
-                User = "Pepe",
-                Clave = "1234",
+                User = user,
+                Clave = clave
             });
 
+            ctx.SaveChanges();
+        }
+
+        static void TareaPost(string titulo, DateTime vto, int estimacion, Recurso responsable, bool estado)
+        {
+            var ctx = new ParcialDBContext();
+
+            ctx.Tareas.Add(new Tarea
+            {
+                Titulo = titulo,
+                Vencimiento = vto,
+                Estimacion = estimacion,
+                Responsable = responsable,
+                Estado = estado
+            });
+
+            ctx.SaveChanges();
+        }
+
+        static void GetUsers()
+        {
+            var ctx = new ParcialDBContext();
+            var lista = ctx.Usuarios.ToList();
+            foreach (var item in lista)
+            {
+                Console.WriteLine($"Nombre: {item.User} ({item.Id})");
+            }
+        }
+
+        static void PutUserByID(int id, string newUser)
+        {
+            var ctx = new ParcialDBContext();
+         
+            var user = ctx.Usuarios.Where(i => i.Id == id).FirstOrDefault();
+            if (user != null)
+            {
+                user.User = newUser;
+            }
+            ctx.SaveChanges();
+        }
+
+        static void PutUserByUsername(string user, string newUser)
+        {
+            var ctx = new ParcialDBContext();
+
+            var row = ctx.Usuarios.Where(i => i.User == user).FirstOrDefault();
+            if (row != null)
+            {
+                row.User = newUser;
+            }
+            ctx.SaveChanges();
+        }
+
+        static void PutUser(int Id, string user, string newUser)
+        {
+            var ctx = new ParcialDBContext();
+
+            var row = ctx.Usuarios.Where(i => i.User == user && i.Id == Id).FirstOrDefault();
+            if (row != null)
+            {
+                row.User = newUser;
+            }
+            ctx.SaveChanges();
+        }
+
+        static void DeleteUser(int id)
+        {
+            var ctx = new ParcialDBContext();
+            var user = ctx.Usuarios.Where(i => i.Id == id).Single();
+            ctx.Usuarios.Remove(user);
             ctx.SaveChanges();
         }
     }
